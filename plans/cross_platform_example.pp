aa969cd176a76d0864a5a64ce8843230dca576c9
Keith Apps <github@apps.org.uk>
2021-12-10 12:36:08 +0000

    Added lnx task and updated plan

--- plans/cross_platform_example.pp
+++ plans/cross_platform_example.pp
@@ -8,21 +8,20 @@ plan tasks_plans_workshop::cross_platform_example(
   out::message('Checking OS version')
 
   # Use puppet agent/facter to determine target OS for further actions below - apply_prep installs puppet agent in dormant state to enable profiling of targets facts via facter (built into puppet agent). 
-    $targets.apply_prep
+  $targets.apply_prep
 
-    $win_targets = get_targets($targets).group_by |$target| { $target.facts['kernel'] }['windows']
-    $nix_targets = get_targets($targets).group_by |$target| { $target.facts['kernel'] }['Linux']
+  $win_targets = get_targets($targets).group_by |$target| { $target.facts['kernel'] }['windows']
+  $nix_targets = get_targets($targets).group_by |$target| { $target.facts['kernel'] }['Linux']
 
   out::message('Running mytask for each OS')
 
   # Basic if else statement to ensure each task runs on a compatible target
-    if ($win_targets) {
-      run_task('tasks_plans_workshop::winsysinfo', $win_targets)
- #     run_command('ipconfig /all', $win_targets)
-    } elsif ($nix_targets) {
-      #run_task('tasks_plans_workshop::mytask', $nix_targets)
-      run_command('ip a', $nix_targets)
-    } else  {
-      fail_plan('OS is not compatible!')
+  if ($win_targets) {
+    run_command('ipconfig /all', $win_targets)
+    run_task('tasks_plans_workshop::winsysinfo', $win_targets)
+  } 
+  if ($nix_targets) {
+    run_command('ip a', $nix_targets)
+    run_task('tasks_plans_workshop::lnxsysinfo', $nix_targets)
   }
 }
--- tasks/winsysinfo.ps1
+++ tasks/winsysinfo.ps1
@@ -1,4 +1,2 @@
-Param($myparam1)
 $systeminfo = systeminfo
 Write-Output "System Info:" $systeminfo
-Write-Output "Your custom parameter was: ${myparam1}"
