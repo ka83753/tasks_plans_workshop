6c7899a42eb4b32c841c67928bb03dfe01efdf56
Keith Apps <github@apps.org.uk>
2021-12-10 14:56:50 +0000

    Removed timesync.pp

--- plans/cross_platform_example.pp
+++ plans/cross_platform_example.pp
@@ -1,4 +1,4 @@
-# @summary This is template plan contains logic to handle cross platform tasks.
+# @summary Simple example plan showing cross platform capability
 # @param targets The targets to run on.
 plan tasks_plans_workshop::cross_platform_example(
   TargetSpec $targets,
@@ -7,7 +7,6 @@ plan tasks_plans_workshop::cross_platform_example(
   run_command('hostname', $targets)
   out::message('Checking OS version')
 
-  # Use puppet agent/facter to determine target OS for further actions below - apply_prep installs puppet agent in dormant state to enable profiling of targets facts via facter (built into puppet agent). 
   $targets.apply_prep
 
   $win_targets = get_targets($targets).group_by |$target| { $target.facts['kernel'] }['windows']
@@ -19,7 +18,7 @@ plan tasks_plans_workshop::cross_platform_example(
   if ($win_targets) {
     run_command('ipconfig /all', $win_targets)
     run_task('tasks_plans_workshop::winsysinfo', $win_targets)
-  } 
+  }
   if ($nix_targets) {
     run_command('ip a', $nix_targets)
     run_task('tasks_plans_workshop::lnxsysinfo', $nix_targets)
